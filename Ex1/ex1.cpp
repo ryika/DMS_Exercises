551df2b30fea000e5881f01200edf37205b6277a
kryomela <63778568+ryika@users.noreply.github.com>
2023-06-15 10:54:53 +0900

    Won/lost message added

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -6,8 +6,14 @@ int main(){
     srand(time(0));
     int dice1 = (rand() % 6) + 1;
     int dice2 = (rand() % 6) + 1;
+    int sum=dice1+dice2;
     cout<<"Rolling dice..."<<endl;
     cout<<"Die 1: "<<dice1<<endl;
     cout<<"Die 2: "<<dice2<<endl;
-    cout<<"Total value: "<<dice1+dice2<<endl;
+    cout<<"Total value: "<<sum<<endl;
+    if(sum>7){
+        cout<<"You won"<<endl;
+    }else{
+        cout<<"You lost"<<endl;
+    }
 }
